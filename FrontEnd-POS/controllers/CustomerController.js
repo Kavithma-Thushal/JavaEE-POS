275875df0d9689a7bb0a7607a76d57eea80ff660
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-01 01:48:34 +0530

    Validations Modified

--- FrontEnd-POS/controllers/CustomerController.js
+++ FrontEnd-POS/controllers/CustomerController.js
@@ -228,52 +228,6 @@ function clearInputFields() {
 }
 
 /**
- * Disable/Enable Buttons
- **/
-function setButtonState(value) {
-    if (value > 0) {
-        $("#btnSaveCustomer").attr('disabled', true);
-        $("#btnUpdateCustomer").attr('disabled', true);
-        $("#btnDeleteCustomer").attr('disabled', true);
-    } else {
-        $("#btnSaveCustomer").attr('disabled', false);
-        $("#btnUpdateCustomer").attr('disabled', false);
-        $("#btnDeleteCustomer").attr('disabled', false);
-    }
-}
-
-/**
- * Customer Validations
- **/
-$("#txtCusId").focus();
-const regExCusID = /^(C00-)[0-9]{3,4}$/;
-const regExCusName = /^[A-z ]{3,20}$/;
-const regExCusAddress = /^[A-z0-9/ ]{4,30}$/;
-const regExSalary = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
-
-let customerValidations = [];
-customerValidations.push({
-    reg: regExCusID,
-    field: $('#txtCusId'),
-    error: 'Customer ID Pattern is Wrong : C00-001'
-});
-customerValidations.push({
-    reg: regExCusName,
-    field: $('#txtCusName'),
-    error: 'Customer Name Pattern is Wrong : A-z 3-20'
-});
-customerValidations.push({
-    reg: regExCusAddress,
-    field: $('#txtCusAddress'),
-    error: 'Customer Address Pattern is Wrong : A-z 0-9 ,/'
-});
-customerValidations.push({
-    reg: regExSalary,
-    field: $('#txtCusSalary'),
-    error: 'Customer Salary Pattern is Wrong : 0-9{1,}.0-9{1,2}'
-});
-
-/**
  * Disable TAB-KEY
  **/
 $("#txtCusId,#txtCusName,#txtCusAddress,#txtCusSalary").on('keydown', function (event) {
