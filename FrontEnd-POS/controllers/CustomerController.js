7fe153e044fb5f07a7678a91c436e24f2eb88be3
Kavithma Thushal <125787087+Thushal2001@users.noreply.github.com>
2024-02-07 15:34:28 +0530

    CustomerController.js Modified 2

--- FrontEnd-POS/controllers/CustomerController.js
+++ FrontEnd-POS/controllers/CustomerController.js
@@ -225,3 +225,93 @@ function clearInputFields() {
     $("#btnUpdateCustomer").attr('disabled', true);
     $("#btnDeleteCustomer").attr('disabled', true);
 }
+
+/**
+ * Customer Validations
+ **/
+const regExCusID = /^(C00-)[0-9]{3,4}$/;
+const regExCusName = /^[A-z ]{3,20}$/;
+const regExCusAddress = /^[A-z0-9/ ]{4,30}$/;
+const regExSalary = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+let customerValidations = [];
+customerValidations.push({
+    reg: regExCusID,
+    field: $('#txtCusId'),
+    error: 'Customer ID Pattern is Wrong : C00-001'
+});
+customerValidations.push({
+    reg: regExCusName,
+    field: $('#txtCusName'),
+    error: 'Customer Name Pattern is Wrong : A-z 3-20'
+});
+customerValidations.push({
+    reg: regExCusAddress,
+    field: $('#txtCusAddress'),
+    error: 'Customer Address Pattern is Wrong : A-z 0-9 ,/'
+});
+customerValidations.push({
+    reg: regExSalary,
+    field: $('#txtCusSalary'),
+    error: 'Customer Salary Pattern is Wrong : 0-9{1,}.0-9{1,2}'
+});
+
+/**
+ * Disable TAB-KEY
+ **/
+$("#txtCusId,#txtCusName,#txtCusAddress,#txtCusSalary").on('keydown', function (event) {
+    if (event.key === "Tab") {
+        event.preventDefault();
+    }
+});
+
+$("#txtCusId,#txtCusName,#txtCusAddress,#txtCusSalary").on('keyup', function (event) {
+    checkValidity(customerValidations);
+});
+
+$("#txtCusId,#txtCusName,#txtCusAddress,#txtCusSalary").on('blur', function (event) {
+    checkValidity(customerValidations);
+});
+
+/**
+ * Enable ENTER-KEY
+ **/
+$("#txtCusId").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExCusID, $("#txtCusId"))) {
+        $("#txtCusName").focus();
+    } else {
+        focusText($("#txtCusId"));
+    }
+});
+$("#txtCusName").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExCusName, $("#txtCusName"))) {
+        focusText($("#txtCusAddress"));
+    }
+});
+$("#txtCusAddress").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExCusAddress, $("#txtCusAddress"))) {
+        focusText($("#txtCusSalary"));
+    }
+});
+$("#txtCusSalary").on('keydown', function (event) {
+    if (event.key === "Enter" && check(regExSalary, $("#txtCusSalary"))) {
+        if (event.which === 13) {
+            $('#btnSaveCustomer').focus();
+        }
+    }
+});
+
+/**
+ * Disable/Enable Buttons
+ **/
+function setButtonState(value) {
+    if (value > 0) {
+        $("#btnSaveCustomer").attr('disabled', true);
+        $("#btnUpdateCustomer").attr('disabled', true);
+        $("#btnDeleteCustomer").attr('disabled', true);
+    } else {
+        $("#btnSaveCustomer").attr('disabled', false);
+        $("#btnUpdateCustomer").attr('disabled', false);
+        $("#btnDeleteCustomer").attr('disabled', false);
+    }
+}
